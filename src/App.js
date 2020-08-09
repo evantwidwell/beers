e379db14ba6d1f1f4b794cdb20890b0b55958b3f
evantwidwell <evanski@gmail.com>
2020-08-08 20:14:24 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import BeerComponent from './beer'
+import LikeComponent from './liked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+constructor(props) {
+super(props)
+this.state = {
+isClicked: false,
+input: '',
+liked: [],
+data: [],
+likedMessage: 'Like this beer?'
+}
 }
 
+componentDidMount() {
+fetch('https://api.punkapi.com/v2/beers/')
+.then(res => res.json())
+.then(examples => {
+console.log(examples);
+this.setState({ data: examples })
+})
+}
+
+likeButton = (beer) =>{
+  let likedList = this.state.liked;
+  likedList.push(beer)
+  this.setState({
+    liked: likedList
+  })
+  console.log(this.state.liked)
+ 
+  return(<h1>
+    "Liked"
+  </h1>)
+ 
+}
+ 
+
+
+render () {
+console.log(this.state.data);
+return (
+<div className="App">
+  <h1>Liked Beers:</h1>
+  <LikeComponent beers = {this.state.liked}/>
+   
+  {this.state.data.map((item, i) => {
+    return (
+
+    <BeerComponent key={i} beer={item} liked={this.likeButton} button={this.state.likedMessage}/>
+)
+})}
+</div>
+
+);
+}
+
+}
 export default App;
